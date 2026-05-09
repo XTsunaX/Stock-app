8bae5223eb1150e8aff5a9d4bb74af80c7a28768
XTsunaX <108163756+XTsunaX@users.noreply.github.com>
2026-05-09 23:20:18 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2199,26 +2199,7 @@ with tab_db:
             components.html(fetch_fubon_html("https://fubon-ebrokerdj.fbs.com.tw/Z/ZG/ZGK_DD.djhtm"), height=600, scrolling=True)
         with inst_tabs[2]:
             components.html(fetch_fubon_html("https://fubon-ebrokerdj.fbs.com.tw/Z/ZG/ZGK_DB.djhtm"), height=600, scrolling=True)
-        st.markdown("---")
-        st.markdown("#### 🔥 三大法人持股變化 (近20檔)")
-        col_buy, col_sell = st.columns(2)
         
-        with col_buy:
-            st.write("📈 **持股增加排行榜**")
-            df_buy = get_tw_stocker_data("buy")
-            if not df_buy.empty:
-                st.dataframe(df_buy, use_container_width=True, hide_index=True)
-            else:
-                st.info("目前無買進資料")
-                
-        with col_sell:
-            st.write("📉 **持股減少排行榜**")
-            df_sell = get_tw_stocker_data("sell")
-            if not df_sell.empty:
-                st.dataframe(df_sell, use_container_width=True, hide_index=True)
-            else:
-                st.info("目前無賣出資料")
-
     with sub_tab2:
         st.markdown("#### 📑 永豐期貨盤後籌碼自動化工具")
         
